44e9f7fd710fc5f159e2d8a4f44a572724861a13
Jarven <769558579@qq.com>
2017-12-20 13:07:36 +0800

    分布式服务注册与发现

---
+++ src/main/java/com/kunlun/CloudEurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.kunlun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+public class CloudEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaServerApplication.class, args);
+    }
+}
