8a721348db79d42d0b0ba588742d610387e9fe08
ycj <532446306@qq.com>
2018-01-09 10:23:35 +0800

    Eureka配置文件

--- src/main/java/com/kunlun/EurekaConfig.java
+++ src/main/java/com/kunlun/EurekaConfig.java
@@ -3,10 +3,14 @@ package com.kunlun;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.logging.log4j2.SpringBootConfigurationFactory;
+import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.cloud.netflix.eureka.server.EurekaServerConfigBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jmx.export.SpringModelMBean;
 
 import java.util.HashMap;
 
@@ -25,6 +29,10 @@ public class EurekaConfig {
     private boolean fetchRegistry;
     @Value("${eureka.client.register-with-eureka}")
     private boolean registerWithEureka;
+    @Value("${eureka.server.enable-self-preservation}")
+    private boolean enableSelfPreservation;
+    @Value("${eureka.server.eviction-interval-timer-in-ms}")
+    private long evictionIntervalTimerInMs;
 
     @Bean
     EurekaClientConfigBean eurekaClientConfigBean() {
@@ -36,4 +44,14 @@ public class EurekaConfig {
         eurekaClientConfigBean.setServiceUrl(clientMap);
         return eurekaClientConfigBean;
     }
+
+    @Bean
+    EurekaServerConfigBean eurekaServerConfigBean() {
+        EurekaServerConfigBean bean = new EurekaServerConfigBean();
+        bean.setEnableSelfPreservation(enableSelfPreservation);
+        bean.setEvictionIntervalTimerInMs(evictionIntervalTimerInMs);
+        return bean;
+    }
+
+
 }
