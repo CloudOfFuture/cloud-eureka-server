ee6f0a90e5b6d0d3f82ec96e3a0b8812073a2a28
ycj <532446306@qq.com>
2018-01-09 10:23:45 +0800

    Eureka配置文件

--- src/main/java/com/kunlun/EurekaConfig.java
+++ src/main/java/com/kunlun/EurekaConfig.java
@@ -1,16 +1,11 @@
 package com.kunlun;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.logging.log4j2.SpringBootConfigurationFactory;
-import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
-import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EurekaServerConfigBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jmx.export.SpringModelMBean;
 
 import java.util.HashMap;
 
